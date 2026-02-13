9d0c73669af4165573fe721d4458afb20a2ebf4b
kolyaboom <tmaloning@gmail.com>
2026-02-13 18:42:44 +0300

    Delete bot directory

--- bot/states.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-
-class UserStates(StatesGroup):
-    ASK_NICK = State()
-    CHOOSE_FORMAT = State()
-    CHOOSE_LIMIT = State()
-    CONFIRM = State()
-
